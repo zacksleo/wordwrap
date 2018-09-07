4bcc750e0131d18418ad4de26dbfbaf3a64f1217
zacksleo <zacksleo@gmail.com>
2018-09-07 23:12:51 +0800

    refact(src): refact wordwrap

--- src/Wordwrap.php
+++ src/Wordwrap.php
@@ -9,34 +9,41 @@ class Wordwrap
         preg_match_all('/\b[\w\s]+\b/', $str, $matches, PREG_OFFSET_CAPTURE);
         $words = array();
         foreach ($matches[0] as $match) {
-            $words[] = array(
-                $match[1] => $match[0]
-            );
+            $words[$match[1]] = $match[0];
         }
+        var_dump($words);
         $chars = preg_split('//u', $str, -1, PREG_SPLIT_NO_EMPTY);
         $res = array();
         $len = 0;
         $line = '';
         $index = 0;
-        foreach ($chars as $char) {
+        $j = 0;
+        foreach ($chars as $i => $char) {
             if ($len >= $width * 2) {
                 $res[] = $line;
                 $line = '';
                 $len = 0;
-                continue;
             }
-            //utf8
-            if (mb_strwidth($char) == 2) {
-                $len += 2;
-            } else {
-                if (in_array($index, array_keys($words)) && $len + mb_strwidth($words[$index]) > $width) {
-                    $res[] = $line;
-                    $line = '';
-                    $len = 0;
-                    continue;
+            //英文单词
+            if (mb_strwidth($char) == 1) {
+                if (in_array($index, array_keys($words))) {
+                    if ($len + mb_strwidth($words[$index]) >= $width * 2) {
+                        $res[] = $line;
+                        $line = '';
+                        $len = 0;
+                        continue;
+                    } else {
+                        $word = $words[$index];
+                        $line .= $word;
+                        $len += mb_strwidth($word);
+                        $index += strlen($word);
+                        continue;
+                    }
                 } else {
                     $len++;
                 }
+            } else {
+                $len += 2;
             }
             $index += strlen($char);
             $line .= $char;
